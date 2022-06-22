d1e29fbdfc778f81c17026763fa39da5c0283bf1
rballage <rballage@student.42.fr>
2022-06-22 09:31:13 +0200

    modified structs names in utils

--- containers/utils.hpp
+++ containers/utils.hpp
@@ -1,13 +1,13 @@
 namespace ft
 {
-	template<bool Cond, class T = void>
+	template<bool condition, class T = void>
 		struct enable_if {};
 
 	template<class T>
 		struct enable_if<true, T> { typedef T type; };
 
 	template <bool is_integral, typename T>
-		struct is_integral_res
+		struct is_integral_struct
 		{
 			typedef T type;
 			static const bool value = is_integral;
@@ -22,55 +22,55 @@ namespace ft
 */
 
 	template <typename>
-	struct is_integral_type : public is_integral_res<false, bool> {};
+	struct is_integral_type : public is_integral_struct<false, bool> {};
 
 	/* @brief is_integral_type for bool. "value is true".*/
 	template <>
-	struct is_integral_type<bool> : public is_integral_res<true, bool> {};
+	struct is_integral_type<bool> : public is_integral_struct<true, bool> {};
 
 	/* @brief is_integral_type for char. "value is true".*/
 	template <>
-	struct is_integral_type<char> : public is_integral_res<true, char> {};
+	struct is_integral_type<char> : public is_integral_struct<true, char> {};
 
 	/* @brief is_integral_type for signed char. "value is true".*/
 	template <>
-	struct is_integral_type<signed char> : public is_integral_res<true, signed char> {};
+	struct is_integral_type<signed char> : public is_integral_struct<true, signed char> {};
 
 	/* @brief is_integral_type for short int. "value is true".*/
 	template <>
-	struct is_integral_type<short int> : public is_integral_res<true, short int> {};
+	struct is_integral_type<short int> : public is_integral_struct<true, short int> {};
 
 	/* @brief is_integral_type for int. "value is true".*/
 	template <>
-	struct is_integral_type<int> : public is_integral_res<true, int> {};
+	struct is_integral_type<int> : public is_integral_struct<true, int> {};
 
 	/* @brief is_integral_type for long int. "value is true".*/
 	template <>
-	struct is_integral_type<long int> : public is_integral_res<true, long int> {};
+	struct is_integral_type<long int> : public is_integral_struct<true, long int> {};
 
 	/* @brief is_integral_type for long long int. "value is true".*/
 	template <>
-	struct is_integral_type<long long int> : public is_integral_res<true, long long int> {};
+	struct is_integral_type<long long int> : public is_integral_struct<true, long long int> {};
 
 	/* @brief is_integral_type for unsigned char. "value is true".*/
 	template <>
-	struct is_integral_type<unsigned char> : public is_integral_res<true, unsigned char> {};
+	struct is_integral_type<unsigned char> : public is_integral_struct<true, unsigned char> {};
 
 	/* @brief is_integral_type for unsigned short int. "value is true".*/
 	template <>
-	struct is_integral_type<unsigned short int> : public is_integral_res<true, unsigned short int> {};
+	struct is_integral_type<unsigned short int> : public is_integral_struct<true, unsigned short int> {};
 
 	/* @brief is_integral_type for unsigned int. "value is true".*/
 	template <>
-	struct is_integral_type<unsigned int> : public is_integral_res<true, unsigned int> {};
+	struct is_integral_type<unsigned int> : public is_integral_struct<true, unsigned int> {};
 
 	/* @brief is_integral_type for unsigned long int. "value is true".*/
 	template <>
-	struct is_integral_type<unsigned long int> : public is_integral_res<true, unsigned long int> {};
+	struct is_integral_type<unsigned long int> : public is_integral_struct<true, unsigned long int> {};
 
 	/* @brief is_integral_type for unsigned long long int. "value is true".*/
 	template <>
-	struct is_integral_type<unsigned long long int> : public is_integral_res<true, unsigned long long int> {};
+	struct is_integral_type<unsigned long long int> : public is_integral_struct<true, unsigned long long int> {};
 
 	/*
 	** @brief Give a structure who contain is the

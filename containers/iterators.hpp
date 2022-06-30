e05ab15b202bb29b017d5cf4d74fffe99afbced0
rballage <rballage@student.42.fr>
2022-06-30 14:36:39 +0200

    stuff in iterators.hpp

--- containers/iterators.hpp
+++ containers/iterators.hpp
@@ -4,3 +4,38 @@
 // 4	Bidirectional Iterator		Same as previous one but can scan the container back and forth;
 // 5	Random Access Iterator		Same as previous one but can access the container also non-sequentially (i.e. by jumping around);
 // 6								Contiguous Iterator	Same as previous one, with the addition that logically adjacent elements are also physically adjacent in memory.
+class iterator {
+public:
+	typedef typename A::difference_type difference_type;
+	typedef typename A::value_type value_type;
+	typedef typename A::reference reference;
+	typedef typename A::pointer pointer;
+	typedef std::random_access_iterator_tag iterator_category; //or another tag
+
+	iterator();
+	iterator(const iterator&);
+	~iterator();
+
+	iterator& operator=(const iterator&);
+	bool operator==(const iterator&) const;
+	bool operator!=(const iterator&) const;
+	bool operator<(const iterator&) const; //optional
+	bool operator>(const iterator&) const; //optional
+	bool operator<=(const iterator&) const; //optional
+	bool operator>=(const iterator&) const; //optional
+
+	iterator& operator++();
+	iterator operator++(int); //optional
+	iterator& operator--(); //optional
+	iterator operator--(int); //optional
+	iterator& operator+=(size_type); //optional
+	iterator operator+(size_type) const; //optional
+	friend iterator operator+(size_type, const iterator&); //optional
+	iterator& operator-=(size_type); //optional
+	iterator operator-(size_type) const; //optional
+	difference_type operator-(iterator) const; //optional
+
+	reference operator*() const;
+	pointer operator->() const;
+	reference operator[](size_type) const; //optional
+};

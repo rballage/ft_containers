afd5ecc64b3407b3e99d4589843395885ed9c4c5
rballage <rballage@student.42.fr>
2022-07-13 08:00:47 +0200

    added real difference_type from iterator_traits in vector

--- vector.hpp
+++ vector.hpp
@@ -10,16 +10,16 @@ namespace ft
 	public:
 		typedef Alloc										allocator_type;
 		typedef T											value_type;
-		typedef std::ptrdiff_t								diff;
 		typedef typename allocator_type::size_type			size_type;
 		typedef typename allocator_type::reference			reference;
 		typedef typename allocator_type::const_reference	const_reference;
 		typedef typename allocator_type::pointer			pointer;
 		typedef typename allocator_type::const_pointer		const_pointer;
-		typedef typename ft::random_access_iterator<value_type>			iterator;
-		typedef typename ft::random_access_iterator<const value_type> 	const_iterator;
-		typedef typename ft::reverse_iterator<iterator>			reverse_iterator;
-		typedef typename ft::reverse_iterator<const_iterator> 		const_reverse_iterator;
+		typedef typename ft::random_access_iterator<value_type>				iterator;
+		typedef typename ft::random_access_iterator<const value_type> 		const_iterator;
+		typedef typename ft::reverse_iterator<iterator>						reverse_iterator;
+		typedef typename ft::reverse_iterator<const_iterator> 				const_reverse_iterator;
+		typedef typename ft::iterator_traits<iterator>::difference_type		difference_type;
 
 	private:
 		pointer _data_start, _data_max, _data_end;

dd8b6d4a617637e7aed8dcd018d6cf12c4a2846c
rballage <rballage@student.42.fr>
2022-07-18 10:12:14 +0200

    implemented pair =, not tested

--- utils.hpp
+++ utils.hpp
@@ -81,4 +81,50 @@ namespace ft
 		}
 		return (first1 == last1) && (first2 != last2);
 	};
+
+	template<typename T1, typename T2>
+	struct pair
+	{
+		typedef T1 first_type;
+		typedef T2 second_type;
+		first_type first;
+		second_type second;
+		pair(void) : first(first_type()), second(second_type()) {};
+		pair(const first_type& a, const second_type& b) : first(a), second(b) {};
+		pair(const first_type& a, const second_type& b) : first(a), second(b) {};
+		template<typename U, typename V>
+		pair(const pair<U,V>& x) : first(x.first), second(x.second) {};
+		pair &operator=(const pair& x) {first = x.first; second = x.second; return *this;};
+
+		template< class U, class V>
+		friend bool operator==( const ft::pair<U, V>& left, const ft::pair<U, V>& right ) {return left.first == right.first && left.second == right.second;};
+		template< class U, class V >
+		friend bool operator<( const ft::pair<U, V>& left, const ft::pair<U, V>& right ) {return left.first < right.first && left.second < right.second;};
+	};
+	template <class T1, class T2>
+	bool operator== (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return lhs.first==rhs.first && lhs.second==rhs.second; }
+
+	template <class T1, class T2>
+	bool operator!= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return !(lhs==rhs); }
+
+	template <class T1, class T2>
+	bool operator<  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return lhs.first<rhs.first || (!(rhs.first<lhs.first) && lhs.second<rhs.second); }
+
+	template <class T1, class T2>
+	bool operator<= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return !(rhs<lhs); }
+
+	template <class T1, class T2>
+	bool operator>  (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return rhs<lhs; }
+
+	template <class T1, class T2>
+	bool operator>= (const pair<T1,T2>& lhs, const pair<T1,T2>& rhs)
+	{ return !(lhs<rhs); }
+
+	template <typename T1, typename T2>
+	pair<T1,T2> make_pair(T1 x, T2 y) {return (pair<T1,T2>(x,y));};
 }
